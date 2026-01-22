509f97b6786247bd18419db0a95d8cd5c519c387
stem-cat <grace@anghelescu.ro>
2026-01-22 17:06:11 -0500

    WORKING straight back pedro pathing. No flywheel yet.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/StraightBackAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/StraightBackAuto.java
@@ -6,10 +6,11 @@ import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.PathChain;
 import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp
+@Autonomous
 public class StraightBackAuto extends OpMode {
     private Follower follower;
     private Timer pathTimer, opModeTimer;
@@ -24,8 +25,8 @@ public class StraightBackAuto extends OpMode {
 
     PathState pathState;
 
-    private final Pose startPose = new Pose(20.392546583850923,122.35527950310559, Math.toRadians(138));
-    private final Pose shootPose = new Pose(59.39875776397515, 83.58260869565218, Math.toRadians(138));
+    private final Pose startPose = new Pose(20.392546583850923,122.35527950310559, Math.toRadians(135));
+    private final Pose shootPose = new Pose(59.39875776397515, 83.58260869565218, Math.toRadians(135));
     private PathChain driveStartPosShootPos;
 
     public void buildPaths() {
@@ -41,25 +42,58 @@ public class StraightBackAuto extends OpMode {
         switch(pathState) {
             case DRIVE_STARTPOS_SHOOTPOS:
                 follower.followPath(driveStartPosShootPos,true);
-                pathState = PathState.SHOOT_PRELOAD;
+                setPathState(PathState.SHOOT_PRELOAD); //reset timer and make new state
                 break;
 
             case SHOOT_PRELOAD:
-                //TODO ADD LOGIC TO THE FLYWHEEL SHOOTER
                 // check is follower done its path
-
-
-
+                if(!follower.isBusy()) {
+                    //TODO ADD LOGIC TO THE FLYWHEEL SHOOTER
+                    telemetry.addLine("Done Path 1");
+                }
+                break;
+            default:
+                telemetry.addLine("No State Commanded");
+                break;
         }
+    }
+
+
+    public void setPathState(PathState newState) {
+        pathState = newState;
+        pathTimer.resetTimer();
+
     }
 
     @Override
     public void init(){
+        pathState = PathState.DRIVE_STARTPOS_SHOOTPOS;
+        pathTimer = new Timer();
+        opModeTimer = new Timer();
+        opModeTimer.resetTimer();
+        follower = Constants.createFollower(hardwareMap);
+        //TODO add in any other init mechanisms
 
+        buildPaths();
+        follower.setPose(startPose);
+
+    }
+
+    public void start() {
+        opModeTimer.resetTimer();
+        setPathState(pathState);
     }
 
     @Override
     public void loop() {
+        follower.update();
+        statePathUpdate();
+
+        telemetry.addData("path state: ", pathState.toString());
+        telemetry.addData("x: ", follower.getPose().getX());
+        telemetry.addData("y: ", follower.getPose().getY());
+        telemetry.addData("heading: ", follower.getPose().getHeading());
+        telemetry.addData("path time: ", pathTimer.getElapsedTimeSeconds());
 
         }
 }
